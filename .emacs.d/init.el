cf40d41a0b7e18e788fd052a288c22f582bfb55e
Shunsuke-Komuta <komuta@level-five.jp>
2016-06-02 20:22:45 +0900

    set core setting of emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,59 @@
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-(setq-default tab-width 4 indent-tabs-mode nil)
-
-(show-paren-mode t)
+;;----------------Visuals------------------;;
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'material t)
+
+(set-face-attribute 'default nil :family "Fira Code" :height 130)
+(setq-default line-spacing 11)
+(tool-bar-mode -1)
+(setq inhibit-startup-message t)
+
+;;----------------Settings-----------------;;
+(show-paren-mode t)
+(setq-default tab-width 4 indent-tabs-mode nil)
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-saved-items 1000)
+
+(icomplete-mode 1)
+
+(add-hook 'slim-mode-hook '(lambda ()
+                             (electric-indent-local-mode -1)))
+
+;;----------------Keybindings---------------;;
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;;----------------Manual Install------------;;
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+;;/
+;;/Smart Compile
+;;/
+(require 'smart-compile)
+(global-set-key (kbd "C-c c") 'smart-compile)
+(global-set-key (kbd "C-c C-c") (kbd "C-c c C-m"))
+(setq compilation-window-height 15)
+
+;;/
+;;/Anything
+;;/
+(add-to-list 'load-path "~/.emacs.d/elisp/anything-config")
+(require 'anything-config)
+(define-key global-map (kbd "C-;") 'anything)
+(setq anything-input-idle-delay 0.1)
+
+;;----------------Package Install------------;;
+(require 'cask "/usr/local/opt/cask/cask.el")
+(cask-initialize)
+
+;;/
+;;/Company-Mode
+;;/
+(require 'company)
+(setq company-idle-delay 0.2)
+(setq company-minimum-prefix-length 3)
+(setq company-selection-wrap-around t)
+(global-company-mode)
+
